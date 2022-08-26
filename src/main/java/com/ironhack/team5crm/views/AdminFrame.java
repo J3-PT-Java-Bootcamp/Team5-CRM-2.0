b52c6d040bacf17fc82d034a3ea6cf81465221d7
isaDevs <isabel.calzadilla.18@gmail.com>
2022-08-26 12:11:45 +0200

    repair the SALESREPFRAMES

--- src/main/java/com/ironhack/team5crm/views/AdminFrame.java
+++ src/main/java/com/ironhack/team5crm/views/AdminFrame.java
@@ -118,6 +118,7 @@ public class AdminFrame extends JFrame implements ActionListener, Operations {
                 }
             }
         }
+        setVisible(false);
         adminSection.setVisible(true);
     }
 
--- src/main/java/com/ironhack/team5crm/views/Operations.java
+++ src/main/java/com/ironhack/team5crm/views/Operations.java
@@ -8,14 +8,14 @@ public interface Operations {
     //  by report SalesRep
     String LEAD_REP = "report lead by salesrep";
     String OPP_REP = "report opportunity by salesrep";
-    String CLOSE_REP = "report close-won by salesrep";
-    String LOST_REP = "report close-lost by salesrep";
+    String CLOSE_REP = "report closed-won by salesrep";
+    String LOST_REP = "report closed-lost by salesrep";
     String OPEN_REP = "report open by salesrep";
 
     //  by report PRODUCT
     String OPPORTUNITY = "report opportunity by ";
-    String CLOSE_WON = "report close-won by ";
-    String CLOSE_LOST = "report close-lost by ";
+    String CLOSE_WON = "report closed-won by ";
+    String CLOSE_LOST = "report closed-lost by ";
 
     String OPEN = "report open by ";
 
